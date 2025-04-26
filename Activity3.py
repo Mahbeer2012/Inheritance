99a65473f8fa964f927c081e4eb73e856bd94153
Mahbeer2012 <mahbeermubashir031@gmail.com>
2025-04-26 16:14:53 +0500

    Activity3.py

---
+++ Activity3.py
@@ -0,0 +1,31 @@
+# parent class
+class Bird:
+
+    def __init__(self):
+        print("Bird is ready")
+
+    def whoisThis(self):
+        print("Bird")
+
+    def swim(self):
+        print("Swim faster")
+
+# child class
+class Penguin(Bird):
+
+    def __init__(self):
+        # call super() function
+        super().__init__()
+        print("Penguin is ready")
+
+    def whoisThis(self):
+        print("Penguin")
+
+def run(self):
+    print("run faster")
+
+# object creation
+peggy = Penguin()
+peggy.whoisthis()
+peggy.swim()
+peggy.run()
